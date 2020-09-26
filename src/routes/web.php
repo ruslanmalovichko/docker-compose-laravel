8515b02abd38722486460bff5a0afbbfd1ddd4c1
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2020-09-26 18:39:04 +0300

    fix(ubuntu): reinstall laravel at ubuntu

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $name = request('name');
-
-    return view('test', [
-      'name' => $name
-    ]);
+    return view('welcome');
 });
